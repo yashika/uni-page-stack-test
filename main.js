7768d183e443155c83d1bbfeb90bbc3db77ea013
maomao9527 <46278492+maomao9527@users.noreply.github.com>
2021-02-11 15:46:34 +0800

    Update main.js
    备注说明

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 Vue.mixin({
 	onLoad(){
-		getApp().globalData.history.push(this.$page.route.slice(-1))
+		getApp().globalData.history.push(this.$page.route.slice(-1)) // 说明:页面被打开就添加一条当前页面的记录到history, 用于日后的链路跟踪
 	}
 })
 const app = new Vue({
